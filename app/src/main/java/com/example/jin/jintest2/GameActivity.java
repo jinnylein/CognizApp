c94ef9fa1209dd8f6ab497be4052d8351abe32b4
Franzi <franziska.lode@de.ibm.com>
2018-07-03 18:45:13 +0200

    Optimized StartFragment

--- app/src/main/java/com/example/jin/jintest2/GameActivity.java
+++ app/src/main/java/com/example/jin/jintest2/GameActivity.java
@@ -2,6 +2,7 @@ package com.example.jin.jintest2;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
